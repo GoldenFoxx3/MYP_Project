cb32385c65190bf3ed0713628043849873b26ef8
Katie <silverstar1872@gmail.com>
2021-06-01 07:00:43 -0400

    Added 3 other corridors and buttons for navigation in school ONLY IN THE FRONT CORRIDOR, uncoded.

---
+++ app/src/main/java/com/example/myp_project/left_corridor.java
@@ -0,0 +1,14 @@
+package com.example.myp_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class left_corridor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_left_corridor);
+    }
+}
---
+++ app/src/main/java/com/example/myp_project/right_corridor.java
@@ -0,0 +1,14 @@
+package com.example.myp_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class right_corridor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_right_corridor);
+    }
+}
